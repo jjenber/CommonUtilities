ab6b89d766038ba77ed1d10f14b194893df2d2dc
jensberg <31927347+jjenber@users.noreply.github.com>
2020-01-03 15:12:02 +0100

    Matrix4x4 tests and function definitions

--- CommonUtilities/Matrix4x4.h
+++ CommonUtilities/Matrix4x4.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <assert.h>
 #include <initializer_list>
+#include "Vector4.h"
+
 namespace CommonUtilities
 {
 	template<typename T>
@@ -11,45 +13,59 @@ namespace CommonUtilities
 		Matrix4x4<T>(const Matrix4x4<T>& aMatrix);
 		Matrix4x4<T>(std::initializer_list<T> aList);
 
+		Matrix4x4<T>& operator=(const Matrix4x4<T>& aMatrix);
+
 		T& operator()(const int aRow, const int aColumn);
 		const T& operator()(const int aRow, const int aColumn) const;
+		bool operator==(const Matrix4x4<T>& aMatrix) const;
+		bool operator!=(const Matrix4x4<T>& aMatrix) const;
+
+		Vector4<T> operator*(const Vector4<T>& aVector);
+		Matrix4x4<T> operator+(const Matrix4x4<T>& aMatrix) const;
+		Matrix4x4<T> operator-(const Matrix4x4<T>& aMatrix) const;
+		Matrix4x4<T> operator*(const Matrix4x4<T>& aMatrix) const;
+		Matrix4x4<T> operator*(const T& aScalar) const;
+		Matrix4x4<T>& operator+=(const Matrix4x4<T>& aMatrix);
+		Matrix4x4<T>& operator-=(const Matrix4x4<T>& aMatrix);
+		Matrix4x4<T>& operator*=(const Matrix4x4<T>& aMatrix);
+		Matrix4x4<T>& operator*=(const T& aScalar);
 
 		static Matrix4x4<T> CreateRotationAroundX(T aAngleInRadians);
 		static Matrix4x4<T> CreateRotationAroundY(T aAngleInRadians);
 		static Matrix4x4<T> CreateRotationAroundZ(T aAngleInRadians);
 		static Matrix4x4<T> Transpose(const Matrix4x4<T>& aMatrixToTranspose);
-
-		Matrix4x4<T> operator+(const Matrix4x4<T>& aMatrix);
-		Matrix4x4<T>& operator+=(const Matrix4x4<T>& aMatrix);
-		Matrix4x4<T> operator-(const Matrix4x4<T>& aMatrix);
-		Matrix4x4<T>& operator-=(const Matrix4x4<T>& aMatrix);
-		Matrix4x4<T> operator*(const Matrix4x4<T>& aMatrix);
-		Matrix4x4<T>& operator*=(const Matrix4x4<T>& aMatrix);
-
 	private:
-		T myData[16];
+		static const size_t myLength = 16;
+		T myData[myLength];
 	};
 
 #pragma region Constructors
 	template <typename T> Matrix4x4<T>::Matrix4x4() : myData() {}
 	template <typename T> Matrix4x4<T>::Matrix4x4(const Matrix4x4<T>& aMatrix)
 	{
-		memcpy(myData, aMatrix.myData, sizeof(T) * size_t(16));
+		memcpy(myData, aMatrix.myData, sizeof(T) * myLength);
 	}
 
 	template<typename T> inline Matrix4x4<T>::Matrix4x4(std::initializer_list<T> aList)
 	{
-		assert(aList.size() < 17 && "Initializer list contains too many values.");
+		assert(aList.size() <= myLength && "Initializer list contains too many values.");
 		std::memcpy(myData, aList.begin(), sizeof(T) * aList.size());
-		if (aList.size() < 16)
+		if (aList.size() < myLength)
 		{
-			std::fill(myData + aList.size(), myData + 16, myData[aList.size() - 1]);
+			std::fill(myData + aList.size(), myData + myLength, myData[aList.size() - 1]);
 		}
 	}
 #pragma endregion Constructors
 
-
 #pragma region Operators
+
+	template<typename T>
+	inline Matrix4x4<T>& Matrix4x4<T>::operator=(const Matrix4x4<T>& aMatrix)
+	{
+		std::memcpy(myData, aMatrix.myData, sizeof(T) * myLength);
+		return *this;
+	}
+
 	// Rows and Columns start at 1.
 	template<typename T> inline T& Matrix4x4<T>::operator()(const int aRow, const int aColumn)
 	{
@@ -60,11 +76,31 @@ namespace CommonUtilities
 	// Rows and Columns start at 1.
 	template<typename T> inline const T& Matrix4x4<T>::operator()(const int aRow, const int aColumn) const
 	{
+		assert(aRow > 0 && aRow < 5 && aColumn > 0 && aColumn < 5 && "Argument out of bounds");
 		return myData[(aRow - 1) + ((aColumn - 1) * 4)];
 	}
 
 	template<typename T>
-	inline Matrix4x4<T> Matrix4x4<T>::operator+(const Matrix4x4<T>& aMatrix)
+	inline bool Matrix4x4<T>::operator==(const Matrix4x4<T>& aMatrix) const
+	{
+		for (int i = 0; i < myLength; i++)
+		{
+			if (myData[i] != aMatrix.myData[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template<typename T>
+	inline bool Matrix4x4<T>::operator!=(const Matrix4x4<T>& aMatrix) const
+	{
+		return !operator==(aMatrix);
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::operator+(const Matrix4x4<T>& aMatrix) const
 	{
 		Matrix4x4<T> result{ *this };
 		return result += aMatrix;
@@ -73,12 +109,155 @@ namespace CommonUtilities
 	template<typename T>
 	inline Matrix4x4<T>& Matrix4x4<T>::operator+=(const Matrix4x4<T>& aMatrix)
 	{
-		for (auto i = 0; i < 16; i++)
+		for (auto i = 0; i < myLength; i++)
 		{
 			myData[i] += aMatrix.myData[i];
 		}
 		return *this;
 	}
 
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::operator-(const Matrix4x4<T>& aMatrix) const
+	{
+		Matrix4x4<T> result{ *this };
+		return result -= aMatrix;
+	}
+
+	template<typename T>
+	inline Matrix4x4<T>& Matrix4x4<T>::operator-=(const Matrix4x4<T>& aMatrix)
+	{
+		for (auto i = 0; i < myLength; i++)
+		{
+			myData[i] -= aMatrix.myData[i];
+		}
+		return *this;
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::operator*(const Matrix4x4<T>& aMatrix) const
+	{
+		Matrix4x4<T> result{ *this };
+		return result *= aMatrix;
+	}
+
+	template<typename T>
+	inline Matrix4x4<T>& Matrix4x4<T>::operator*=(const Matrix4x4<T>& aMatrix)
+	{
+		Matrix4x4<T> result;
+		for (int i = 1; i <= 4; i++)
+		{
+			for (auto j = 1; j <= 4; j++)
+			{
+				T product{ 0 };
+				for (auto k = 1; k <= 4; k++)
+				{
+					product += this->operator()(k, i) * aMatrix(j, k);
+				}
+				result(j, i) = product;
+			}
+		}
+		std::memcpy(this->myData, result.myData, sizeof(T) * myLength);
+		return *this;
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::operator*(const T& aScalar) const
+	{
+		Matrix4x4<T> result{ *this };
+		return result *= aScalar;
+	}
+	template<typename T>
+	inline Matrix4x4<T>& Matrix4x4<T>::operator*=(const T& aScalar)
+	{
+		for (int i = 0; i < myLength; i++)
+		{
+			myData[i] *= aScalar;
+		}
+		return *this;
+	}
+
+	template<typename T>
+	inline Vector4<T> Matrix4x4<T>::operator*(const Vector4<T>& aVector)
+	{
+		Vector4<T> result;
+		for (int i = 0; i < 4; i++)
+		{
+			result.x += myData[i * 4] * aVector.x;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			result.y += myData[i * 4 + 1] * aVector.y;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			result.z += myData[i * 4 + 2] * aVector.z;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			result.w += myData[i * 4 + 3] * aVector.w;
+		}
+		return result;
+	}
+
 #pragma endregion Operators
+
+#pragma region Static Functions
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::CreateRotationAroundX(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			T(1), 0,  0,  0,
+			0,  cos, sin, 0,
+			0, -sin, cos, 0,
+			0,  0,  0,  T(1)
+		};
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::CreateRotationAroundY(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			cos, 0, -sin, 0,
+			0, T(1), 0,   0,
+			sin, 0,  cos, 0,
+			0,   0,  0, T(1)
+		};
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::CreateRotationAroundZ(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			cos,  sin, 0, 0,
+			-sin, cos, 0, 0,
+			0,    0, T(1),0,
+			0,    0, 0, T(1)
+		};
+	}
+
+	template<typename T>
+	inline Matrix4x4<T> Matrix4x4<T>::Transpose(const Matrix4x4<T>& aMatrixToTranspose)
+	{
+		Matrix4x4<T> result;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				result.myData[i + j * 4] = aMatrixToTranspose.myData[j + i * 4];
+			}
+		}
+		return result;
+	}
+
+#pragma endregion Static Functions
 }
