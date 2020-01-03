5680561873b3783039bfbffc7aabae94a24fd800
jensberg <31927347+jjenber@users.noreply.github.com>
2020-01-03 13:56:16 +0100

    Add static roation and transpose functions to 3x3.

--- CommonUtilities/Matrix3x3.h
+++ CommonUtilities/Matrix3x3.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <assert.h>
+#include <cmath>
 #include "Matrix4x4.h"
+#include "Vector3.h"
 namespace CommonUtilities
 {
 	template<typename T> 
@@ -12,29 +14,31 @@ namespace CommonUtilities
 		Matrix3x3<T>(std::initializer_list<T> aList);
 		Matrix3x3<T>(const Matrix4x4<T>& aMatrix);
 
+		Matrix3x3<T>& operator=(const Matrix3x3<T>& aMatrix);
+	
 		T& operator()(const int aRow, const int aColumn);
 		const T& operator()(const int aRow, const int aColumn) const;
-	
+		bool operator==(const Matrix3x3<T>& aMatrix) const;
+		bool operator!=(const Matrix3x3<T>& aMatrix) const;
+
+		Vector3<T> operator*(const Vector3<T>& aVector);
+		Matrix3x3<T> operator+(const Matrix3x3<T>& aMatrix) const;
+		Matrix3x3<T> operator-(const Matrix3x3<T>& aMatrix) const;
+		Matrix3x3<T> operator*(const Matrix3x3<T>& aMatrix) const;
+		Matrix3x3<T> operator*(const T& aScalar) const;
+		Matrix3x3<T>& operator+=(const Matrix3x3<T>& aMatrix);
+		Matrix3x3<T>& operator-=(const Matrix3x3<T>& aMatrix);
+		Matrix3x3<T>& operator*=(const Matrix3x3<T>& aMatrix);
+		Matrix3x3<T>& operator*=(const T& aScalar);
+		
 		static Matrix3x3<T> CreateRotationAroundX(const T aAngleInRadians);
 		static Matrix3x3<T> CreateRotationAroundY(const T aAngleInRadians);
 		static Matrix3x3<T> CreateRotationAroundZ(const T aAngleInRadians);
 		static Matrix3x3<T> Transpose(const Matrix3x3<T>& aMatrixToTranspose);
-
-		Matrix3x3<T> operator+(const Matrix3x3<T>& aMatrix);
-		Matrix3x3<T>& operator+=(const Matrix3x3<T>& aMatrix);
-		Matrix3x3<T> operator-(const Matrix3x3<T>& aMatrix);
-		Matrix3x3<T>& operator-=(const Matrix3x3<T>& aMatrix);
-		Matrix3x3<T> operator*(const Matrix3x3<T>& aMatrix);
-		Matrix3x3<T>& operator*=(const Matrix3x3<T>& aMatrix);
-
-		Matrix3x3<T> operator*(const T& aScalar);
-		Matrix3x3<T>& operator*=(const T& aScalar);
-	
 	private:
 		T myData[9];
 	};
 	
-
 #pragma region Constructors
 	template <typename T> Matrix3x3<T>::Matrix3x3() : myData() {}
 	template <typename T> Matrix3x3<T>::Matrix3x3(const Matrix3x3<T>& aMatrix)
@@ -55,14 +59,23 @@ namespace CommonUtilities
 	template<typename T>
 	inline Matrix3x3<T>::Matrix3x3(const Matrix4x4<T>& aMatrix)
 	{
-		std::memcpy(myData,     &aMatrix(1, 1), sizeof(T) * 3);
-		std::memcpy(myData + 3, &aMatrix(1, 2), sizeof(T) * 3);
-		std::memcpy(myData + 6, &aMatrix(1, 3), sizeof(T) * 3);
+		const size_t length = sizeof(T) * 3;
+		std::memcpy(myData,     &aMatrix(1, 1), length);
+		std::memcpy(myData + 3, &aMatrix(1, 2), length);
+		std::memcpy(myData + 6, &aMatrix(1, 3), length);
 	}
 
 #pragma endregion Constructors
 
 #pragma region Operators
+
+	template<typename T>
+	inline Matrix3x3<T>& Matrix3x3<T>::operator=(const Matrix3x3<T>& aMatrix)
+	{
+		std::memcpy(myData, aMatrix.myData, sizeof(T) * 9);
+		return *this;
+	}
+
 	// Rows and Columns start at 1.
 	template<typename T> inline T& Matrix3x3<T>::operator()(const int aRow, const int aColumn)
 	{
@@ -77,7 +90,26 @@ namespace CommonUtilities
 	}
 
 	template<typename T>
-	inline Matrix3x3<T> Matrix3x3<T>::operator+(const Matrix3x3<T>& aMatrix)
+	inline bool Matrix3x3<T>::operator==(const Matrix3x3<T>& aMatrix) const
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			if (myData[i] != aMatrix.myData[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template<typename T>
+	inline bool Matrix3x3<T>::operator!=(const Matrix3x3<T>& aMatrix) const
+	{
+		return !operator==(aMatrix);
+	}
+
+	template<typename T>
+	inline Matrix3x3<T> Matrix3x3<T>::operator+(const Matrix3x3<T>& aMatrix) const
 	{
 		Matrix3x3<T> result{ *this };
 		return result += aMatrix;
@@ -101,7 +133,7 @@ namespace CommonUtilities
 	}
 
 	template<typename T>
-	inline Matrix3x3<T> Matrix3x3<T>::operator-(const Matrix3x3<T>& aMatrix)
+	inline Matrix3x3<T> Matrix3x3<T>::operator-(const Matrix3x3<T>& aMatrix) const
 	{
 		Matrix3x3<T> result{ *this };
 		return result -= aMatrix;
@@ -118,7 +150,7 @@ namespace CommonUtilities
 	}
 
 	template<typename T>
-	inline Matrix3x3<T> Matrix3x3<T>::operator*(const Matrix3x3<T>& aMatrix)
+	inline Matrix3x3<T> Matrix3x3<T>::operator*(const Matrix3x3<T>& aMatrix) const
 	{
 		Matrix3x3<T> result{ *this };
 		return result *= aMatrix;
@@ -132,12 +164,12 @@ namespace CommonUtilities
 		{
 			for (auto j = 1; j <= 3; j++)
 			{
-				T value{ 0 };
+				T product{ 0 };
 				for (auto k = 1; k <= 3; k++)
 				{
-					value += this->operator()(k, i) * aMatrix(j, k);
+					product += this->operator()(k, i) * aMatrix(j, k);
 				}
-				result(j, i) = value;
+				result(j, i) = product;
 			}
 		}
 		std::memcpy(this->myData, result.myData, sizeof(T) * 9);
@@ -145,7 +177,7 @@ namespace CommonUtilities
 	}
 
 	template<typename T>
-	inline Matrix3x3<T> Matrix3x3<T>::operator*(const T& aScalar)
+	inline Matrix3x3<T> Matrix3x3<T>::operator*(const T& aScalar) const
 	{
 		Matrix3x3<T> result{ *this };
 		return result *= aScalar;
@@ -160,6 +192,81 @@ namespace CommonUtilities
 		return *this;
 	}
 
+	template<typename T>
+	inline Vector3<T> Matrix3x3<T>::operator*(const Vector3<T>& aVector)
+	{
+		Vector3<T> result;
+		for (int i = 0; i < 3; i++)
+		{
+			result.x += myData[i * 3] * aVector.x;
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			result.y += myData[i * 3 + 1] * aVector.y;
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			result.z += myData[i * 3 + 2] * aVector.z;
+		}
+		return result;
+	}
 
 #pragma endregion Operators
+
+#pragma region Static Functions
+
+	template<typename T>
+	inline Matrix3x3<T> Matrix3x3<T>::CreateRotationAroundX(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			T(1), 0, 0,
+			0, cos, sin,
+			0, -sin, cos
+		};
+	}
+
+	template<typename T>
+	inline Matrix3x3<T> Matrix3x3<T>::CreateRotationAroundY(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			cos, 0, -sin,
+			0, T(1), 0,
+			sin, 0, cos
+		};
+	}
+
+	template<typename T>
+	inline Matrix3x3<T> Matrix3x3<T>::CreateRotationAroundZ(const T aAngleInRadians)
+	{
+		const T cos = std::cos(aAngleInRadians);
+		const T sin = std::sin(aAngleInRadians);
+		return
+		{
+			cos,  sin, 0,
+			-sin, cos, 0,
+			0,    0, T(1)
+		};
+	}
+
+	template<typename T>
+	inline Matrix3x3<T> Matrix3x3<T>::Transpose(const Matrix3x3<T>& aMatrixToTranspose)
+	{
+		Matrix3x3<T> result;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				result.myData[i + j * 3] = aMatrixToTranspose.myData[j + i * 3];
+			}
+		}
+		return result;
+	}
+
+#pragma endregion Static Functions
 }
--- UnitTests/MatrixTests.cpp
+++ UnitTests/MatrixTests.cpp
@@ -32,6 +32,23 @@ namespace MatrixTests
 			Assert::AreEqual(5.f, copy(1, 1));
 		}
 
+		TEST_METHOD(Assignment)
+		{
+			CommonUtilities::Matrix3x3<float> mat{ 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
+			CommonUtilities::Matrix3x3<float> copy{ 2.f };
+			copy = mat;
+			Assert::AreEqual(1.f, mat(1, 1));
+			Assert::AreEqual(2.f, mat(2, 1));
+			Assert::AreEqual(4.f, mat(1, 2));
+			Assert::AreEqual(5.f, mat(2, 2));
+			Assert::AreEqual(9.f, mat(3, 3));
+
+			Assert::AreEqual(1.f, copy(1, 1));
+			Assert::AreEqual(2.f, copy(2, 1));
+			Assert::AreEqual(4.f, copy(1, 2));
+			Assert::AreEqual(5.f, copy(2, 2));
+			Assert::AreEqual(9.f, copy(3, 3));
+		}
 		TEST_METHOD(ListInitializationFull)
 		{
 			CommonUtilities::Matrix3x3<float> mat{ 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
@@ -122,6 +139,95 @@ namespace MatrixTests
 			Assert::AreEqual(6, compoundResult(1, 1));
 			Assert::AreEqual(4, compoundResult(3, 2));
 			Assert::AreEqual(2, compoundResult(3, 3));
+		}
+		TEST_METHOD(OperatorMultiplicationVector3)
+		{
+			CommonUtilities::Matrix3x3<int> mat{
+				3, 3, 4,
+				2, 6, 2,
+				1, 1, 1
+			};
+			CommonUtilities::Vector3<int> vector{ 1, 2, 3 };
+			auto result = mat * vector;
+
+			Assert::AreEqual(6, result.x);
+			Assert::AreEqual(20, result.y);
+			Assert::AreEqual(21, result.z);
+		}
+
+		TEST_METHOD(Equality)
+		{
+			CommonUtilities::Matrix3x3<float> mat{ 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
+			CommonUtilities::Matrix3x3<float> copy{ 2.f };
+			if (copy == mat)
+			{
+				Assert::Fail(L"Equality failed");
+			}
+
+			copy = mat;
+			if (copy != mat)
+			{
+				Assert::Fail(L"Equality failed");
+			}
+		}
+
+		TEST_METHOD(TransposeMat)
+		{
+			CommonUtilities::Matrix3x3<int> mat{
+				1, 2, 3,
+				4, 5, 6,
+				7, 8, 9
+			};
+
+			CommonUtilities::Matrix3x3<int> transposed{
+				1, 4, 7,
+				2, 5, 8,
+				3, 6, 9
+			};
+
+			auto res = CommonUtilities::Matrix3x3<int>::Transpose(mat);
+			Assert::AreEqual(transposed(1, 1), res(1, 1));
+			Assert::AreEqual(transposed(2, 1), res(2, 1));
+			Assert::AreEqual(transposed(3, 1), res(3, 1));
+
+			Assert::AreEqual(transposed(1, 2), res(1, 2));
+			Assert::AreEqual(transposed(2, 2), res(2, 2));
+			Assert::AreEqual(transposed(3, 2), res(3, 2));
+
+			Assert::AreEqual(transposed(1, 3), res(1, 3));
+			Assert::AreEqual(transposed(2, 3), res(2, 3));
+			Assert::AreEqual(transposed(3, 3), res(3, 3));
+		}
+
+		TEST_METHOD(RotationAroundX)
+		{
+			CommonUtilities::Matrix3x3<float> mat{
+				1.f, 2.f, 3.f,
+				4.f, 5.f, 6.f,
+				7.f, 8.f, 9.f
+			};
+
+			auto rotation = CommonUtilities::Matrix3x3<float>::CreateRotationAroundX(0.5f);
+		}
+		TEST_METHOD(RotationAroundY)
+		{
+			CommonUtilities::Matrix3x3<float> mat{
+				1.f, 2.f, 3.f,
+				4.f, 5.f, 6.f,
+				7.f, 8.f, 9.f
+			};
+
+			auto rotation = CommonUtilities::Matrix3x3<float>::CreateRotationAroundY(0.5f);
+		}
+		TEST_METHOD(RotationAroundZ)
+		{
+			CommonUtilities::Matrix3x3<float> mat{
+				1.f, 2.f, 3.f,
+				4.f, 5.f, 6.f,
+				7.f, 8.f, 9.f
+			};
+
+			auto rotation = CommonUtilities::Matrix3x3<float>::CreateRotationAroundZ(0.5f);
 		}
 	};
 	TEST_CLASS(Matrix4x4)
