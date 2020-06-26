93969eebf3ea634a94558924740cc5b59322536e
jensberg <31927347+jjenber@users.noreply.github.com>
2020-06-26 11:26:38 +0200

    Delete created test files after completion.

--- Source/UnitTests/JsonUtilityTests.cpp
+++ Source/UnitTests/JsonUtilityTests.cpp
@@ -8,25 +8,51 @@ namespace Json
 {
 	TEST_CLASS(JsonUtilityTest)
 	{
-		TEST_METHOD(OpenAndRead)
+		TEST_METHOD(Write)
 		{
 			JsonUtility file;
-			bool open =  file.Open("jsontest1.json", false);
+			bool open = file.Open("jsonWriteTest.json", true);
+			auto d = file.GetParseErrorMessage();
 			Assert::IsTrue(open);
-
-			int i = file.ParseValue<int>("Test");
-			Assert::AreEqual(2, i);
+			
+			file.WriteValue("teststring", 7);
+			file.WriteValue("teststring2", 8);
+			file.WriteValue("TestingAString", std::string("Testing"));
+			file.WriteValue("TestingAStringAgain", std::string("Testing2"));
+			file.WriteValue("TestingAStringAgain2", std::string("Testing2"));
+			const char* s = "d";
+			file.WriteValue("TestingLiteral", s);
+			file.WriteValue("TestingLiteralInline", "inline");
+			file.SaveDocument();
+			file.Delete();
 		}
-		TEST_METHOD(OpenAndReadArray)
+		TEST_METHOD(Erase)
 		{
 			JsonUtility file;
-			bool open = file.Open("jsontest1.json", false);
-			std::string msg = file.GetParseErrorMessage();
-			Assert::IsTrue(open);
+			bool open = file.Open("jsonWriteTest.json", true);
+			file.WriteValue("testinteger", 7);
 
-			file.IterateOverArray("Arr", [](const rapidjson::Value& element) {
-				Assert::IsTrue(element.IsInt());
-				});
+			Assert::IsTrue(file.HasMember("testinteger"));
+			file.EraseValue("testinteger");
+		}
+		TEST_METHOD(Clear)
+		{
+			JsonUtility file;
+			bool open = file.Open("jsonWriteTest.json", true);
+			file.WriteValue("testinteger", 7);
+
+			Assert::IsTrue(file.HasMember("testinteger"));
+			file.Clear();
+		}
+		TEST_METHOD(Delete)
+		{
+			JsonUtility file;
+			file.Open("jsontest.json", true);
+			file.Delete();
+
+			JsonUtility file1;
+			file1.Open("jsonWriteTest.json", true);
+			
 		}
 	};
 }
